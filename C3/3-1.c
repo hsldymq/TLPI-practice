a8772a29ace6e34580d94208b47d8bc48d6e043c
longhaibin <longhaibin@juwang.cn>
2018-03-13 23:13:19 +0800

    update

--- C3/3-1.c
+++ C3/3-1.c
@@ -1,19 +1,8 @@
 #include <unistd.h>
 #include <linux/reboot.h>
 #include <stdio.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <stdlib.h>
-#include <string.h>
 
 int main() {
-    int fd = open("hashdf", O_RDONLY);
-    if (fd == -1) {
-        perror("open file");
-        printf("%s\n", strerror(errno));
-        exit(EXIT_FAILURE);
-    }
-
     printf("LINUX_REBOOT_MAGIC1: 0x%08X\n", LINUX_REBOOT_MAGIC1);
     printf("LINUX_REBOOT_MAGIC2: 0x%08X\n", LINUX_REBOOT_MAGIC2);
     printf("LINUX_REBOOT_MAGIC2A: 0x%08X\n", LINUX_REBOOT_MAGIC2A);
